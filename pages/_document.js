a06d3b587fbd135082b3b399cf26743f365d416f
Drew Bredvick <dbredvick@gmail.com>
2021-08-20 23:33:27 -0500

    feat: add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            defer
+            data-website-id="f45d511d-686a-4038-af10-de8b73c955cc"
+            src="https://data.drew.tech/umami.js"
+          ></script>
         </Head>
         <body>
           <Main />
